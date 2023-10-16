4e62a37849c1313bda7731e2c6df85639b88501c
Yusra Aftab <yusraaftab642@gmail.com>
2023-10-16 01:08:59 -0700

    third push

---
+++ backend/authentication/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+]
+
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
